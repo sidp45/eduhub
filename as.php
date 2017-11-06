c80f3428d16df71d52fa59a0593dc8368d4535c7
sidp45 <33368720+sidp45@users.noreply.github.com>
2017-11-06 20:41:37 +0530

    Update as.php
    Registration form validations added

--- as.php
+++ as.php
@@ -1,20 +1,33 @@
 <?php
 include("db.php");
 if(isset($_REQUEST["submit"])){
-$name=$_REQUEST["fn"];
-$email=$_REQUEST["eml"];
-$password=$_REQUEST["pwd"];
-$repassword=$_REQUEST["repwd"];
-if($password==$repassword){
-	$sql="insert into registration (userName,email,password) values('$name','$email','$password')";
-	mysqli_query($conn,$sql);
-	echo "<script>alert('succefully registered');</script>";
-	header("Location:ma.php");
-}
-else
-{
-echo "<script>alert('password does not match');</script>";}
-
+	$name=$_REQUEST["fn"];
+	$email=$_REQUEST["eml"];
+	$password=$_REQUEST["pwd"];
+	$repassword=$_REQUEST["repwd"];
+	if($name==NULL)
+	{
+		echo "<script>alert('Kindly fill the name.');</script>";
+	}
+	else
+	if($email==NULL){
+		echo "<script>alert('Kindly fill the email.');</script>";
+	}
+	else
+	if($password==NULL){
+		echo "<script>alert('Kindly fill the password.');</script>";
+	}
+	else
+	if($password==$repassword){
+		$sql="insert into registration (userName,email,password) values('$name','$email','$password')";
+		mysqli_query($conn,$sql);
+		echo "<script>alert('succefully registered');</script>";
+		header("Location:ma.php");
+	}
+	else
+	{
+	echo "<script>alert('password does not match');</script>";
+	}
 }
 ?>
 
